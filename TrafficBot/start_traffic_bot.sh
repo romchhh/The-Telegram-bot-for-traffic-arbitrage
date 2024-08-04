440e9251e7a50835f91551f51e65c0220a6b80da
nowayrm <123520267+romchhh@users.noreply.github.com>
2024-08-04 23:22:07 +0300

    Delete TrafficBot directory

--- TrafficBot/start_traffic_bot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /root/TrafficBot/myenv/bin/activate
-nohup python3 /root/TrafficBot/main.py > /dev/null 2>&1 &
--- TrafficBot/states/user_states.py
+++
@@ -1,4 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-class CardState(StatesGroup):
-    waiting_for_card = State()
